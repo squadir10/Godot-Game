8513accaaf594a8b90f7f8eac78015fadbafbe22
Sabrina Q. <97366704+squadir10@users.noreply.github.com>
2023-12-24 22:37:24 -0500

    Coded left and right controls

--- scenes/Pink_Man.cs
+++ scenes/Pink_Man.cs
@@ -3,13 +3,24 @@ using System;
 
 public partial class Pink_Man : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 400.0f;
+	public const float JumpVelocity = -600.0f;
+	private AnimatedSprite2D Sprite2D;
+
+	public override void _Ready()
+    {
+        Sprite2D = GetNode<AnimatedSprite2D>("Sprite2D");
+		GD.Print(Sprite2D);
+    }
+
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
-	public override void _PhysicsProcess(double delta)
+	
+
+    
+    public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
@@ -21,12 +32,21 @@ public partial class Pink_Man : CharacterBody2D
 		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
+		
+
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+
+		// Only change the velocity and flip the sprite if there is input.
+		if (Input.IsActionPressed("ui_right")){
+			velocity.X = Speed;
+			Sprite2D.FlipH = false; //face right
+		}
+		else if (Input.IsActionPressed ("ui_left"))
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = -Speed;
+			Sprite2D.FlipH = true; //Face left
 		}
 		else
 		{
@@ -35,5 +55,8 @@ public partial class Pink_Man : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
+
+		
+		
 	}
 }
